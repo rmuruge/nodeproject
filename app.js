3ad3bdde8fd0edb293672f644724cf56db04fb87
rmuruge <rmuruge@gmail.com>
2019-09-24 04:11:27 +0000

    updated

--- app.js
+++ app.js
@@ -1,18 +1,27 @@
 const express = require('express');
 const app = express();
 const router = express.Router();
-const db = require('./db');
 const sharks = require('./routes/sharks');
 
 const path = __dirname + '/views/';
-const port = 8080;
+const port = 8088;
 
-app.engine('html', require('ejs').renderFile);
-app.set('view engine', 'html');
-app.use(express.urlencoded({ extended: true }));
+router.use(function (req,res,next) {
+  console.log('/' + req.method);
+  next();
+});
+
+router.get('/', function(req,res){
+  res.sendFile(path + 'index.html');
+});
+
+router.get('/sharks', function(req,res){
+  res.sendFile(path + 'sharks.html');
+});
+
 app.use(express.static(path));
-app.use('/sharks', sharks);
+app.use('/', router);
 
 app.listen(port, function () {
-  console.log('Example app listening on port 8080!')
+  console.log('Example app listening on port ' + port)
 })
